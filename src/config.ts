73ca2e1fa5131b1247fa8414a2a62956a5a0e2b7
angelolima <angelomiguellima@gmail.com>
2025-02-12 23:03:55 +0100

    Define and enforce a Config interface in config.ts
    Introduced a Config interface to explicitly define the structure of the configuration object. This improves type safety and ensures consistency in the expected fields and their types. Updated the `logLevel` property to enforce stricter types for allowed values.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
-export const config = {
+interface Config {
+    modelFilPath: string,
+    iaModelName: string,
+    logLevel: 'info' | 'debug' | 'warn' | 'error',
+}
+
+export const config: Config = {
     modelFilPath: process.env.MODEL_PATH ?? '',
     iaModelName: process.env.IA_MODEL_NAME ?? '',
-    logLevel: process.env.LOG_LEVEL ?? 'info'
+    logLevel: (process.env.LOG_LEVEL as 'info' | 'debug' | 'warn' | 'error') ?? 'info'
 }
